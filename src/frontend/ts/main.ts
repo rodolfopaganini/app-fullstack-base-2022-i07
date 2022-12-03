ae0a2852c15c390d8f990b81f4fa5cee03f47a80
Rodolfo Paganini <rodolfo.paganini@mercadolibre.com>
2022-12-03 10:17:14 -0300

    change greeting to alert

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,10 @@
+window.onload = start;
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+function start(){
+    let btn = document.getElementById("btnGreet");
+    btn.onclick = greet;
+}
+
+function greet() {
+    alert("Hi");
 }
